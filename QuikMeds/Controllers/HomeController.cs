a20a1298c2bf3c449fbee5b7d48348ed2a162c67
renjithkumars <renjith.sk@outlook.com>
2019-06-16 07:08:43 +0100

    test 2

--- QuikMeds/Controllers/HomeController.cs
+++ QuikMeds/Controllers/HomeController.cs
@@ -8,7 +8,10 @@ namespace QuikMeds.Controllers
 {
     public class HomeController : Controller
     {
-       
+        public ActionResult Index()
+        {
+            return View();
+        }
 
         public ActionResult About()
         {
