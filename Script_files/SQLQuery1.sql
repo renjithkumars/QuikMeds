9c046666fa55142a022911f1fd5da9f5338b1e3b
renjithkumars <renjith.sk@outlook.com>
2019-06-16 07:02:35 +0100

    6/16/2019 Script files

---
+++ Script_files/SQLQuery1.sql
@@ -0,0 +1,32 @@
+USE [CTX]
+GO
+
+/****** Object:  Table [dbo].[Orders]    Script Date: 6/16/2019 6:52:15 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[DeliveryDate] [datetime] NOT NULL,
+	[CID] [int] NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CID])
+REFERENCES [dbo].[Customers] ([CID])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
+
+
