76e109343616347c81e9ad3d9ec4c87fdfde0dfd
renjithkumars <renjith.sk@outlook.com>
2019-06-22 13:15:31 +0100

    1.15 pm 6/22/19

---
+++ QuikMeds/Controllers/CheckoutController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuikMeds.Controllers
+{
+    public class CheckoutController : BaseController
+    {
+
+
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            ViewBag.Cart = _ctx.ShoppingCartDatas.ToList<ShoppingCartData>();
+            return View();
+        }
+
+        
+
+        [HttpGet]
+        public JsonResult UpdateTotal()
+        {
+            CTXEntities context = new CTXEntities();
+            decimal total;
+            try
+            {
+
+                total = context.ShoppingCartDatas.Select(p => p.UnitPrice * p.Quantity).Sum();
+            }
+            catch (Exception) { total = 0; }
+
+            return Json(new { d = String.Format("{0:c}", total) }, JsonRequestBehavior.AllowGet);
+        }
+
+        
+
+
+    }
+}
