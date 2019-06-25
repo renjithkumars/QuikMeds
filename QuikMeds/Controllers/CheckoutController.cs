e7ac7d4bcbf58a66c99afb6773fc32c54d9cc363
renjithkumars <renjith.sk@outlook.com>
2019-06-26 00:09:59 +0100

    12.09 am 6/26/19

--- QuikMeds/Controllers/CheckoutController.cs
+++ QuikMeds/Controllers/CheckoutController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
 
 namespace QuikMeds.Controllers
 {
@@ -184,8 +188,47 @@ namespace QuikMeds.Controllers
                     }
 
                     _ctx.SaveChanges();
+                   //Email
+                    string result = null;
+                    
+                    try
+                    {
+                        MailMessage mailMessage = new MailMessage("shilpa.eis2@gmail.com ", c.Email);
+                        mailMessage.Subject = "new order";
+                        mailMessage.Body = ("Thank you for using QuiKMeds.......    Your ordenumber : " + o.OrderID+ "   Order date :  "+o.OrderDate+"  Delivery date  : "+o.DeliveryDate);
+                        mailMessage.IsBodyHtml = false;
 
-                    return RedirectToAction("PurchasedSuccess");
+                        SmtpClient smtp = new SmtpClient();
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+
+                        NetworkCredential nc = new NetworkCredential("shilpa.eis2@gmail.com", "Assignment");
+                        smtp.UseDefaultCredentials = true;
+
+                        smtp.Credentials = nc;
+                        smtp.Send(mailMessage);
+                        ViewBag.Message = "Mail has been sent successfuly";
+
+                        if (ModelState.IsValid)
+                        {
+
+                            //saving
+                            if (string.IsNullOrEmpty(result))
+                            {
+                                return RedirectToAction("Index");
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        ViewBag.Message = "Message Error!!! Please enter a valid Email Id";
+                        return RedirectToAction("Index");
+                    }
+
+                    return RedirectToAction("Index", "Home");
+               
+
 
                 }
             }
